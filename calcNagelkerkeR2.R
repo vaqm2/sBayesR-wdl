0fc4fc39c968e3e72113e28d608e4a9b42b8bb3e
Vivek Appadurai <vivekappadurai@viveks-mbp.lan>
2020-04-28 12:22:17 +0200

    Rscript now works!

--- calcNagelkerkeR2.R
+++ calcNagelkerkeR2.R
@@ -3,23 +3,48 @@
 require(dplyr)
 require(fmsb)
 
-args        = commandArgs(trailingOnly = F)
+args        = commandArgs(trailingOnly = TRUE)
 fileOfFiles = args[1]
 prefix      = args[2]
-out         = data.frame("ScoreFile", "VarianceExplained")
+out         = paste0(prefix, ".VarianceExplained.txt")
+fileStream  = file(fileOfFiles, "r")
 
-fileStream = file(fileOfFiles, "r")
+file.create(out)
+sink(out)
+
+cat("File")
+cat(" ")
+cat("r2")
+cat(" ")
+cat("P")
+cat(" ")
+cat("N")
+cat("\n")
+
+while(TRUE)
 {
-  scoreFile = readLines(fileStream, n = 1)
-  if(length(scoreFile) == 0)
-  {
-    break
-  }
-  scores = read.table(scoreFile, header = TRUE)
-  logReg = glm(data = scores, PHENO1 ~ SCORE1_AVG)
-  r2     = NagelkerkeR2(logReg)
-  rbind(out, cbind(scoreFile, r2))
-}
-close(fileStream)
+    scoreFile = readLines(fileStream, n = 1)
 
-write.table(out, paste(prefix, ".VarianceExplained.txt", row.names = F, quote = F))
+    if(length(scoreFile) == 0)
+    {
+        break
+    }
+    else
+    {
+        scores = read.table(scoreFile, header = TRUE, comment.char = "")
+        logReg = glm(data = scores, PHENO1 ~ SCORE1_AVG)
+        r2     = NagelkerkeR2(logReg)
+
+        cat(gsub("^.*/", "", scoreFile))
+        cat(" ")
+        cat(r2$R2)
+        cat(" ")
+        cat(coef(summary(logReg))[2,4])
+        cat(" ")
+        cat(r2$N)
+        cat("\n")
+    }
+}
+
+sink()
+close(fileStream)
